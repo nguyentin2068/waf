10fce0fcf5fa8b11112c2d3c38506d9d2711aaf1
tinnt2 <nguyentin2068@gmail.com>
2023-07-25 14:17:15 +0700

    update

--- internal/operators/validateopenapi.go
+++ internal/operators/validateopenapi.go
@@ -23,6 +23,7 @@ func newValidateOpenAPI(plugintypes.OperatorOptions) (plugintypes.Operator, erro
 
 func (o *validateOpenAPI) Evaluate(_ plugintypes.TransactionState, value string) bool {
 	schemaFile := "/opt/APISchema/api.json"
+	print(value)
 	reqe := strings.Split(value, " ")
 	methd := reqe[0]
 	uri := reqe[1]
